af6b1f32df18266c2ce0f6b9565c2dabe7caef28
ahpmatias <63597072+ahpmatias@users.noreply.github.com>
2024-11-12 17:59:16 +0100

    Finish exercises introducing control statements

---
+++ ControlStatementsIntro/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int age = 15;
+	cout << age << endl;
+
+	if (age >= 16)
+	{
+		cout << "You can drive!" << endl;
+	}
+	else
+	{
+		cout << "You cannot drive yet!" << endl;
+	}
+
+	//for loop
+	for (int i = 1; i <= age; i++)
+	{
+		cout << "Happy birthday " << i << endl;
+	}
+	return 0;
+}
