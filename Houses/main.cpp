90646e20262f24808df158b2dc24f66284d19352
ahpmatias <63597072+ahpmatias@users.noreply.github.com>
2024-11-27 11:31:32 +0100

    Finish separate compilation lesson.

--- Houses/main.cpp
+++ Houses/main.cpp
@@ -1,46 +1,8 @@
 #include <iostream>
 #include <string>
+#include "House.h"
+
 using namespace std;
-
-class House
-{
-	public:
-		void setNumStories(int numStories)
-		{
-			this->numStories = numStories;
-		}
-
-		void setNumWindows(int numWindows)
-		{
-			this->numWindows = numWindows;
-		}
-
-		void setColor(string color)
-		{
-			this->color = color;
-		}
-
-		int getNumStories() const
-		{
-			return numStories;
-		}
-
-		int getNumWindows() const
-		{
-			return numWindows;
-		}
-
-		string getColor() const
-		{
-			return color;
-		}
-	private:
-		int numStories;
-		int numWindows;
-		string color;
-};
-
-void printHouseData(const House& houseObject);
 
 int main()
 {
@@ -55,15 +17,9 @@ int main()
 	yourHouse.setNumWindows(10);
 	yourHouse.setColor("blue");
 
-	printHouseData(myHouse);
-	printHouseData(yourHouse);
+	myHouse.print();
+	yourHouse.print();
 
 	return 0;
 }
 
-void printHouseData(const House& object)
-{
-	cout << "The house is " << object.getColor() << ", has "
-		 << object.getNumStories() << " stories and "
-		 << object.getNumWindows() << " windows." << endl;
-}
