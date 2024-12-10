f8395faee18ee458bead54370f32fe45d9f3465a
ahpmatias <63597072+ahpmatias@users.noreply.github.com>
2024-12-10 21:15:35 +0100

    Finish custom exception lesson.

---
+++ CustomExceptions/AngryCatException.h
@@ -0,0 +1,25 @@
+#ifndef ANGRY_CAT_EXCEPTION_H
+#define ANGRY_CAT_EXCEPTION_H
+
+#include <stdexcept>
+
+using namespace std;
+
+class AngryCatException : public runtime_error
+{
+	public:
+		AngryCatException() : runtime_error("Made kitty angry!")
+		{
+
+		}
+
+		AngryCatException(const string& err) : runtime_error(err)
+		{
+
+		}
+
+	private:
+		
+};
+
+#endif // !ANGRY_CAT_EXCEPTION_H
